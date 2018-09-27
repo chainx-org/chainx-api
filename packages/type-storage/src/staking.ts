f861322b9ff33543eea16744318f1a365c077a4f
yznt <nsso@foxmail.com>
2018-09-27 11:13:22 +0800

    Staking

--- packages/type-storage/src/staking.ts
+++ packages/type-storage/src/staking.ts
@@ -8,277 +8,179 @@ import { Storages, Storage$Sections } from './types';
 import param from '@polkadot/params/param';
 import createSection from '@polkadot/params/section';
 
-const bondingDuration: CreateItemOptions = {
-  description: 'The length of the bonding duration in eras',
-  key: 'sta:loc',
-  params: [],
-  type: 'BlockNumber'
-};
-
 const validatorCount: CreateItemOptions = {
-  description: 'The ideal number of staking participants',
-  key: 'sta:vac',
+  description: `The ideal number of staking participants.`,
+  key: 'Staking ValidatorCount',
   params: [],
   type: 'u32'
 };
 
 const minimumValidatorCount: CreateItemOptions = {
-  description: 'Minimum number of staking participants before emergency conditions are imposed',
-  key: 'sta:minimum_validator_count',
+  description: `Minimum number of staking participants before emergency conditions are imposed.`,
+  key: 'Staking MinimumValidatorCount',
   params: [],
   type: 'u32'
 };
 
 const sessionsPerEra: CreateItemOptions = {
-  description: 'The length of a staking era in sessions',
-  key: 'sta:spe',
+  description: `The length of a staking era in sessions.`,
+  key: 'Staking SessionsPerEra',
   params: [],
   type: 'BlockNumber'
 };
 
-const totalStake: CreateItemOptions = {
-  description: 'The total amount of stake on the system',
-  key: 'sta:tot',
-  params: [],
-  type: 'Balance'
-};
-
-const transactionBaseFee: CreateItemOptions = {
-  description: 'The fee to be paid for making a transaction; the base',
-  key: 'sta:basefee',
-  params: [],
-  type: 'Balance'
-};
-
-const transactionByteFee: CreateItemOptions = {
-  description: 'The fee to be paid for making a transaction; the per-byte portion',
-  key: 'sta:bytefee',
-  params: [],
-  type: 'Balance'
-};
-
-const existentialDeposit: CreateItemOptions = {
-  description: 'The minimum amount allowed to keep an account open',
-  key: 'sta:existential_deposit',
-  params: [],
-  type: 'Balance'
-};
-
-const reclaimRebate: CreateItemOptions = {
-  description: 'The amount credited to a destination account whose index was reclaimed',
-  key: 'sta:reclaim_rebate',
-  params: [],
-  type: 'Balance'
-};
-
-const transferFee: CreateItemOptions = {
-  description: 'The fee required to make a transfer',
-  key: 'sta:transfer_fee',
-  params: [],
-  type: 'Balance'
-};
-
-const creationFee: CreateItemOptions = {
-  description: 'The fee required to create an account',
-  key: 'sta:creation_fee',
-  params: [],
-  type: 'Balance'
-};
-
 const sessionReward: CreateItemOptions = {
-  description: 'Maximum reward, per validator, that is provided per acceptable session',
-  key: 'sta:early_era_slash',
+  description: `Maximum reward, per validator, that is provided per acceptable session.`,
+  key: 'Staking SessionReward',
   params: [],
   type: 'Balance'
 };
 
-const earlyEraSlash: CreateItemOptions = {
-  description: 'Slash, per validator that is taken per abnormal era end',
-  key: 'sta:early_era_slash',
+const offlineSlash: CreateItemOptions = {
+  description: `Slash, per validator that is taken for the first time they are found to be offline.`,
+  key: 'Staking OfflineSlash',
   params: [],
   type: 'Balance'
 };
 
 const offlineSlashGrace: CreateItemOptions = {
-  description: 'Number of instances of offline reports before slashing begins for validators',
-  key: 'sta:offline_slash_grace',
+  description: `Number of instances of offline reports before slashing begins for validators.`,
+  key: 'Staking OfflineSlashGrace',
   params: [],
   type: 'u32'
 };
 
-const currentEra: CreateItemOptions = {
-  description: 'The current era index',
-  key: 'sta:era',
+const bondingDuration: CreateItemOptions = {
+  description: `The length of the bonding duration in blocks.`,
+  key: 'Staking BondingDuration',
   params: [],
   type: 'BlockNumber'
 };
 
-const slashPreferenceOf: CreateItemOptions = {
-  description: 'How many times the validator should get slashed for being offline and automatically unstaked',
-  key: 'sta:slash_preference_of',
-  params: [
-    param('who', 'AccountId')
-  ],
-  type: 'u32'
+const currentEra: CreateItemOptions = {
+  description: `The current era index.`,
+  key: 'Staking CurrentEra',
+  params: [],
+  type: 'BlockNumber'
+};
+
+const validatorPreferences: CreateItemOptions = {
+  description: `Preferences that a validator has.`,
+  key: 'Staking ValidatorPreferences',
+  params: [param('who', 'AccountId')],
+  type: ['Balance']
 };
 
 const intentions: CreateItemOptions = {
-  description: 'All the accounts with a desire to stake',
-  key: 'sta:wil:',
+  description: `All the accounts with a desire to stake.`,
+  key: 'Staking Intentions',
   params: [],
   type: ['AccountId']
 };
 
 const nominating: CreateItemOptions = {
-  description: 'Nominator -> nominee relationship',
-  key: 'sta:nominating',
-  params: [
-    param('who', 'AccountId')
-  ],
+  description: `All nominator -> nominee relationships.`,
+  key: 'Staking Nominating',
+  params: [param('who', 'AccountId')],
   type: 'AccountId'
 };
 
 const nominatorsFor: CreateItemOptions = {
-  description: 'Nominators for a particular account',
-  key: 'sta:nominators_for',
-  params: [
-    param('who', 'AccountId')
-  ],
+  description: `Nominators for a particular account.`,
+  key: 'Staking NominatorsFor',
+  params: [param('who', 'AccountId')],
   type: ['AccountId']
 };
 
 const currentNominatorsFor: CreateItemOptions = {
-  description: 'Nominators for a particular account that is in action right now',
-  key: 'sta:current_nominators_for',
-  params: [
-    param('who', 'AccountId')
-  ],
+  description: `Nominators for a particular account that is in action right now.`,
+  key: 'Staking CurrentNominatorsFor',
+  params: [param('who', 'AccountId')],
   type: ['AccountId']
 };
 
+const currentSessionReward: CreateItemOptions = {
+  description: `Maximum reward, per validator, that is provided per acceptable session.`,
+  key: 'Staking CurrentSessionReward',
+  params: [],
+  type: 'Balance'
+};
+
+const currentOfflineSlash: CreateItemOptions = {
+  description: `Slash, per validator that is taken for the first time they are found to be offline.`,
+  key: 'Staking CurrentOfflineSlash',
+  params: [],
+  type: 'Balance'
+};
+
 const nextSessionsPerEra: CreateItemOptions = {
-  description: 'The next value of sessions per era',
-  key: 'sta:nse',
+  description: `The next value of sessions per era.`,
+  key: 'Staking NextSessionsPerEra',
   params: [],
   type: 'BlockNumber'
 };
 
 const lastEraLengthChange: CreateItemOptions = {
-  description: 'The session index at which the era length last changed',
-  key: 'sta:lec',
+  description: `The session index at which the era length last changed.`,
+  key: 'Staking LastEraLengthChange',
   params: [],
   type: 'BlockNumber'
 };
 
-const stakeThreshold: CreateItemOptions = {
-  description: 'The current era stake threshold',
-  key: 'sta:stake_threshold',
+// @TODO pairOf
+const stakeRange: CreateItemOptions = {
+  description: `The highest and lowest staked validator slashable balances.`,
+  key: 'Staking StakeRange',
   params: [],
-  type: 'Balance'
+  type: ['Balance']
 };
 
-const nextEnumSet: CreateItemOptions = {
-  description: 'The next free enumeration set',
-  key: 'sta:next_enum',
-  params: [],
-  type: 'AccountIndex'
+const bondage: CreateItemOptions = {
+  description: "The block at which the `who`'s funds become entirely liquid.",
+  key: 'Staking Bondage',
+  params: [param('who', 'AccountId')],
+  type: 'BlockNumber'
 };
 
-const enumSet: CreateItemOptions = {
-  description: 'The enumeration sets',
-  key: 'sta:enum_set',
-  params: [
-    param('index', 'AccountIndex')
-  ],
-  type: ['AccountId']
+const slashCount: CreateItemOptions = {
+  description: `The number of times a given validator has been reported offline. This gets decremented by one each era that passes.`,
+  key: 'Staking SlashCount',
+  params: [param('who', 'AccountId')],
+  type: 'u32'
 };
 
-const freeBalanceOf: CreateItemOptions = {
-  description: 'The balance of a given account',
-  key: 'sta:bal:',
-  params: [
-    param('who', 'AccountId')
-  ],
-  type: 'Balance'
-};
-
-const reservedBalanceOf: CreateItemOptions = {
-  description: 'The amount of a given account that is reserved',
-  key: 'sta:lbo:',
-  params: [
-    param('who', 'AccountId')
-  ],
-  type: 'Balance'
-};
-
-const bondageOf: CreateItemOptions = {
-  key: 'sta:bon:',
-  description: 'The block at which the account becomes liquid',
-  params: [
-    param('who', 'AccountId')
-  ],
-  type: 'Balance'
-};
+// @TODO
+// const forcingNewEra: CreateItemOptions = {
+//   description: `We are forcing a new era.`,
+//   key: 'Staking ForcingNewEra',
+//   params: [],
+//   type: ''
+// };
 
 export default (name: Storage$Sections): Section<Storages, any, any> =>
   createSection(name)((createMethod: CreateItems<Storages>) => ({
     description: 'Staking',
     public: {
-      freeBalanceOf:
-        createMethod('freeBalanceOf')(freeBalanceOf),
-      bondageOf:
-        createMethod('bondageOf')(bondageOf),
-      creationFee:
-        createMethod('creationFee')(creationFee),
-      currentNominatorsFor:
-        createMethod('currentNominatorsFor')(currentNominatorsFor),
-      earlyEraSlash:
-        createMethod('earlyEraSlash')(earlyEraSlash),
-      existentialDeposit:
-        createMethod('existentialDeposit')(existentialDeposit),
-      enumSet:
-        createMethod('enumSet')(enumSet),
-      transactionBaseFee:
-        createMethod('transactionBaseFee')(transactionBaseFee),
-      transactionByteFee:
-        createMethod('transactionByteFee')(transactionByteFee),
-      transferFee:
-        createMethod('transferFee')(transferFee),
-      bondingDuration:
-        createMethod('bondingDuration')(bondingDuration),
-      currentEra:
-        createMethod('currentEra')(currentEra),
-      reservedBalanceOf:
-        createMethod('reservedBalanceOf')(reservedBalanceOf),
-      intentions:
-        createMethod('intentions')(intentions),
-      lastEraLengthChange:
-        createMethod('lastEraLengthChange')(lastEraLengthChange),
-      minimumValidatorCount:
-        createMethod('minimumValidatorCount')(minimumValidatorCount),
-      nextEnumSet:
-        createMethod('nextEnumSet')(nextEnumSet),
-      nextSessionsPerEra:
-        createMethod('nextSessionsPerEra')(nextSessionsPerEra),
-      nominating:
-        createMethod('nominating')(nominating),
-      nominatorsFor:
-        createMethod('nominatorsFor')(nominatorsFor),
-      offlineSlashGrace:
-        createMethod('offlineSlashGrace')(offlineSlashGrace),
-      reclaimRebate:
-        createMethod('reclaimRebate')(reclaimRebate),
-      slashPreferenceOf:
-        createMethod('slashPreferenceOf')(slashPreferenceOf),
-      stakeThreshold:
-        createMethod('stakeThreshold')(stakeThreshold),
-      sessionsPerEra:
-        createMethod('sessionsPerEra')(sessionsPerEra),
-      sessionReward:
-        createMethod('sessionReward')(sessionReward),
-      totalStake:
-        createMethod('totalStake')(totalStake),
-      validatorCount:
-        createMethod('validatorCount')(validatorCount)
+      validatorCount: createMethod('validatorCount')(validatorCount),
+      minimumValidatorCount: createMethod('minimumValidatorCount')(minimumValidatorCount),
+      sessionsPerEra: createMethod('sessionsPerEra')(sessionsPerEra),
+      sessionReward: createMethod('sessionReward')(sessionReward),
+      offlineSlash: createMethod('offlineSlash')(offlineSlash),
+      offlineSlashGrace: createMethod('offlineSlashGrace')(offlineSlashGrace),
+      bondingDuration: createMethod('bondingDuration')(bondingDuration),
+      currentEra: createMethod('currentEra')(currentEra),
+      validatorPreferences: createMethod('validatorPreferences')(validatorPreferences),
+      intentions: createMethod('intentions')(intentions),
+      nominating: createMethod('nominating')(nominating),
+      nominatorsFor: createMethod('nominatorsFor')(nominatorsFor),
+      currentNominatorsFor: createMethod('currentNominatorsFor')(currentNominatorsFor),
+      currentSessionReward: createMethod('currentSessionReward')(currentSessionReward),
+      currentOfflineSlash: createMethod('currentOfflineSlash')(currentOfflineSlash),
+      nextSessionsPerEra: createMethod('nextSessionsPerEra')(nextSessionsPerEra),
+      lastEraLengthChange: createMethod('lastEraLengthChange')(lastEraLengthChange),
+      stakeRange: createMethod('stakeRange')(stakeRange),
+      bondage: createMethod('bondage')(bondage),
+      slashCount: createMethod('slashCount')(slashCount)
+      // forcingNewEra: createMethod('forcingNewEra')(forcingNewEra)
     }
   }));
